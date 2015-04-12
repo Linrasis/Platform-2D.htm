87b7f77586d80364be2e723f92c32d8830664ace
Jan Horava <czechinthemail@gmail.com>
2015-04-12 10:21:39 -0700

    improved formatting

--- js/platform-2d.js
+++ js/platform-2d.js
@@ -493,29 +493,30 @@ function setmode(newmode, newgame){
           settings['ms-per-frame']
         );
 
-    // Main menu mode.
-    }else{
-        buffer = 0;
-        buffer_static = 0;
-        canvas = 0;
-
-        world_dynamic.length = 0;
-        world_static.length = 0;
-        world_text.length = 0;
-
-        document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick=setmode(3,1)>Generate Random Level</a><br><a onclick=setmode(4,1)>Randomized Lava Corridor</a></div><hr><div class=c><a onclick=setmode(5,1)>A Pit of Your Design</a><br><a onclick=setmode(6,1)>Booster Towers</a><br><a onclick=setmode(7,1)>Keys of a Father</a><br><a onclick=setmode(8,1)>Tutorial Island</a><br><a onclick=setmode(9,1)>Village of the Wolves</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input id=jump-key maxlength=1 value='
-          + settings['jump-key'] + '>Jump<br><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
-          + settings['movement-keys'] + '>Move ←→<br><input id=restart-key maxlength=1 value='
-          + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
-          + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
-          + settings['color'] + '>Color<br><input id=gravity value='
-          + settings['gravity'] + '>Gravity<br><input id=jump-speed value='
-          + settings['jump-speed'] + '>Jump Speed<br><input id=ms-per-frame value='
-          + settings['ms-per-frame'] + '>ms/Frame<br><input id=speed value='
-          + settings['speed'] + '>Speed<br><input id=terminal-velocity value='
-          + settings['terminal-velocity'] + '>Terminal Velocity<br><label><input'
-          + (settings['time-display'] ? ' checked' : '') + ' id=time-display type=checkbox>Time</label><br><a onclick=reset()>Reset Settings</a></div></div>';
+        return;
     }
+
+    // Main menu mode.
+    buffer = 0;
+    buffer_static = 0;
+    canvas = 0;
+
+    world_dynamic.length = 0;
+    world_static.length = 0;
+    world_text.length = 0;
+
+    document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick="setmode(3, true)">Generate Random Level</a><br><a onclick="setmode(4, true)">Randomized Lava Corridor</a></div><hr><div class=c><a onclick="setmode(5, true)">A Pit of Your Design</a><br><a onclick="setmode(6, true)">Booster Towers</a><br><a onclick="setmode(7, true)">Keys of a Father</a><br><a onclick="setmode(8, true)">Tutorial Island</a><br><a onclick="setmode(9, true)">Village of the Wolves</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input id=jump-key maxlength=1 value='
+      + settings['jump-key'] + '>Jump<br><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
+      + settings['movement-keys'] + '>Move ←→<br><input id=restart-key maxlength=1 value='
+      + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
+      + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
+      + settings['color'] + '>Color<br><input id=gravity value='
+      + settings['gravity'] + '>Gravity<br><input id=jump-speed value='
+      + settings['jump-speed'] + '>Jump Speed<br><input id=ms-per-frame value='
+      + settings['ms-per-frame'] + '>ms/Frame<br><input id=speed value='
+      + settings['speed'] + '>Speed<br><input id=terminal-velocity value='
+      + settings['terminal-velocity'] + '>Terminal Velocity<br><label><input'
+      + (settings['time-display'] ? ' checked' : '') + ' id=time-display type=checkbox>Time</label><br><a onclick=reset()>Reset Settings</a></div></div>';
 }
 
 function update_static_buffer(){
@@ -743,7 +744,10 @@ window.onkeydown = function(e){
 
     // ESC: return to main menu.
     if(key === 27){
-        setmode(0, 1);
+        setmode(
+          0,
+          true
+        );
         return;
     }
 
@@ -759,7 +763,10 @@ window.onkeydown = function(e){
         key_jump = true;
 
     }else if(key === settings['restart-key']){
-        setmode(mode, 0);
+        setmode(
+          mode,
+          false
+        );
     }
 };
 
@@ -779,7 +786,10 @@ window.onkeyup = function(e){
 };
 
 window.onload = function(e){
-    setmode(0, 1);
+    setmode(
+      0,
+      true
+    );
 };
 
 window.onresize = resize;
