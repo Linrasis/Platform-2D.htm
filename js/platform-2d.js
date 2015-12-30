79d839c941cd1094bea1d9c21f159c564d2e5570
Jan Horava <czechinthemail@gmail.com>
2015-12-29 23:01:20 -0800

    simplified save()

--- js/platform-2d.js
+++ js/platform-2d.js
@@ -403,13 +403,13 @@ function save(){
     };
     for(var id in ids){
         var value = document.getElementById(id).value;
+        settings[id] = parseFloat(value);
+
         if(value == ids[id]
           || isNaN(value)){
             window.localStorage.removeItem('Platform-2D.htm-' + id);
-            settings[id] = ids[id];
 
         }else{
-            settings[id] = parseFloat(value);
             window.localStorage.setItem(
               'Platform-2D.htm-' + id,
               settings[id]
@@ -436,12 +436,12 @@ function save(){
     };
     for(id in ids){
         value = document.getElementById(id).value;
+        settings[id] = value;
+
         if(value === ids[id]){
             window.localStorage.removeItem('Platform-2D.htm-' + id);
-            settings[id] = ids[id];
 
         }else{
-            settings[id] = value;
             window.localStorage.setItem(
               'Platform-2D.htm-' + id,
               settings[id]
